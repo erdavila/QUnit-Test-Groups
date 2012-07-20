36cbf75ddeb5beebc40dc1baf92572fc0e7fd0cb
Eduardo R. D'Avila <erdavila@gmail.com>
2012-07-20 00:13:15 -0300

    Moving files to example directory.

--- example/tests-groups.js
+++ example/tests-groups.js
@@ -1,7 +1,7 @@
-var testA1 = { url : "test-A1.js", name : "A1" };
-var testA2 = { url : "test-A2.js", name : "A2" };
-var testB1 = { url : "test-B1.js", name : "B1" };
-var testB2 = { url : "test-B2.js", name : "B2" };
+var testA1 = { url : "../example/test-A1.js", name : "A1" };
+var testA2 = { url : "../example/test-A2.js", name : "A2" };
+var testB1 = { url : "../example/test-B1.js", name : "B1" };
+var testB2 = { url : "../example/test-B2.js", name : "B2" };
 
 var testsA = new TestGroup("Tests A", [
 	testA1,
